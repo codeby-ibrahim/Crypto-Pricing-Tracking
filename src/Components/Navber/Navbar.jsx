0ddb03bc17cd37c7175854d8f72f24cf8c3d78c6
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-07-15 12:18:48 +0500

    added

---
+++ src/Components/Navber/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css'
+import './assets/Navbar.css'
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={logo} alt="" />
+        
+
+    </div>
+  )
+}
+
+export default Navbar
