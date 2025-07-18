a0716f5da36aded96ad194115aef3bac2204a17b
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-07-18 10:45:50 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import Navbar from './Components/Navber/Navbar'
 import {Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home'
 import Coin from './pages/Coin/Coin'
+import Footer from './pages/Footer/Footer';
 const App = () => {
   return (
     <div className='app'>
         <Navbar/>
        < Routes>
          <Route path='/' element={<Home/>}/>
-       <Route path='/coin/:coinid' element={<Coin/>}/>  
-       
-
+       <Route path='/coin/:coinid' element={<Coin/>}/> 
        </ Routes>
+       <Footer/>
     </div>
   )
 }
