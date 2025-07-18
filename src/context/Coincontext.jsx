93400275da298c6b03c13936d05986fc9ca8d01e
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-07-18 10:00:32 +0500

    added

--- src/context/Coincontext.jsx
+++ src/context/Coincontext.jsx
@@ -9,24 +9,18 @@ const CoinContextProvider = ({ children }) => {
     symbol: "$"
   });
 
-  const fetchAllCoin = async () => {
-    try {
-      const response = await fetch(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`,
-        {
-          method: 'GET',
-          headers: {
-            accept: 'application/json',
-            'x-cg-demo-api-key': 'CG-91Na3gF37jLkMimFB9B4Ftwp'
-          }
-        }
-      );
-      const data = await response.json();
-      setAllCoin(data);
-    } catch (error) {
-      console.error("Error fetching coins:", error);
-    }
-  };
+const fetchAllCoin = async () => {
+  try {
+    const response = await fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`
+    );
+    const data = await response.json();
+    setAllCoin(data);
+  } catch (error) {
+    console.error("Error fetching coins:", error);
+  }
+};
+
 
   useEffect(() => {
     fetchAllCoin();
